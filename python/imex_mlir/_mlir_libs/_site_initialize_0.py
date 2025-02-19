e6203997ea980c87b8fdfe4410456106608c3dd5
Tuomas Karna <tuomas.karna@intel.com>
2025-02-19 10:37:05 +0200

    add imex C API and python bindings
    python bindings enabled via IMEX_ENABLE_BINDINGS_PYTHON cmake option

---
+++ python/imex_mlir/_mlir_libs/_site_initialize_0.py
@@ -0,0 +1,12 @@
+# ===-- _site_initialize_0.py - For site init -----------------*- Python -*-===#
+#
+# This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+# ===-----------------------------------------------------------------------===#
+
+
+def context_init_hook(context):
+    # this will invoke imex binding registrations
+    from imex_mlir.dialects.region import EnvironmentRegionOp  # noqa
